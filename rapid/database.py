3a7a43084fa2f3044d6ae01058fd212c7837a700
Rick de Harder <rick.de.harder@valcon.com>
2024-05-03 09:39:19 +0200

    Add testing database functionallity

---
+++ rapid/database.py
@@ -0,0 +1,10 @@
+from sqlmodel import create_engine, Session
+
+sqlite_file_name = "rapid_db.db"
+sqlite_url = f"sqlite:///{sqlite_file_name}"
+engine = create_engine(sqlite_url, echo=True)
+
+
+def get_session():
+    with Session(engine) as session:
+        yield session
