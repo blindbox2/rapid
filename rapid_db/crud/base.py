2d914eb7eb29ca61259aa4e4958bf14041e3066b
Rick de Harder <rick.de.harder@valcon.com>
2024-05-05 18:09:59 +0200

    Added logging to the crud functionallity

--- rapid_db/crud/base.py
+++ rapid_db/crud/base.py
@@ -1,5 +1,9 @@
 from sqlmodel import Session, select
 from typing import Generic, TypeVar, List
+import logging
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.WARNING)
 
 ModelType = TypeVar("ModelType", bound="SQLModel")
 CreateSchemaType = TypeVar("CreateSchemaType", bound="SQLModel")
@@ -7,49 +11,71 @@ UpdateSchemaType = TypeVar("UpdateSchemaType", bound="SQLModel")
 ReturnSchemaType = TypeVar("ReturnSchemaType", bound="SQLModel")
 
 
-class GenericCrud(Generic[ModelType, CreateSchemaType, UpdateSchemaType, ReturnSchemaType]):
+class GenericCrud(
+    Generic[ModelType, CreateSchemaType, UpdateSchemaType, ReturnSchemaType]
+):
     def __init__(self, model: ModelType):
         self.model = model
         self.name = model.__tablename__
 
-    def create_model(self, session: Session, model: CreateSchemaType) -> ReturnSchemaType:
+    def create_model(
+        self, session: Session, model: CreateSchemaType
+    ) -> ReturnSchemaType:
+        logger.info(f"Creating new {self.name} record")
         db_model = self.model.model_validate(model)
         session.add(db_model)
         session.commit()
         session.refresh(db_model)
+        logger.info(f"Created new {self.name} record with ID: {db_model.id}")
         return db_model
 
-    def get_all_models(self, session: Session, offset: int = 0, limit: int = 100) -> List[ReturnSchemaType]:
-        models = session.exec(
-            select(self.model).offset(offset).limit(limit)).all()
+    def get_all_models(
+        self, session: Session, offset: int = 0, limit: int = 100
+    ) -> List[ReturnSchemaType]:
+        logger.info(
+            f"Getting all {self.name} records with offset: {offset} and limit: {limit}"
+        )
+        models = session.exec(select(self.model).offset(offset).limit(limit)).all()
         if len(models) == 0:
+            logger.warning(f"No {self.name} records found")
             raise ValueError(f"404: no {self.name} found.")
         return models
 
     def get_model_on_id(self, session: Session, model_id: int) -> ReturnSchemaType:
         db_model = session.get(self.model, model_id)
         if not db_model:
-            raise ValueError(
-                f"404: {self.name[:-1]} with ID: {model_id} not found.")
+            logger.warning(f"{self.name[:-1]} record with ID: {model_id} not found")
+            raise ValueError(f"404: {self.name[:-1]} with ID: {model_id} not found.")
         return db_model
 
-    def update_model(self, session: Session, model_id: int, model: UpdateSchemaType) -> ReturnSchemaType:
+    def update_model(
+        self, session: Session, model_id: int, model: UpdateSchemaType
+    ) -> ReturnSchemaType:
+        logger.info(f"Updating {self.name[:-1]} record with ID: {model_id}")
         db_model = self.get_model_on_id(session, model_id)
         model_data = model.model_dump(exclude_unset=True)
         db_model.sqlmodel_update(model_data)
         session.add(db_model)
         session.commit()
         session.refresh(db_model)
+        logger.info(f"Updated {self.name[:-1]} record with ID: {model_id}")
         return db_model
 
-    def delete_model(self, session: Session, model_id: int, hard_delete: bool = False) -> dict:
+    def delete_model(
+        self, session: Session, model_id: int, hard_delete: bool = False
+    ) -> dict:
+        logger.info(f"Deleting {self.name[:-1]} record with ID: {model_id}")
         db_model = self.get_model_on_id(session, model_id)
 
         if hard_delete:
+            logging.info(
+                f"Executing hard delete for {self.name[:-1]} record with ID: {model_id}"
+            )
             session.delete(db_model)
         else:
             db_model.is_active = False
             session.add(db_model)
 
         session.commit()
+        logger.info(f"Deleted {self.name[:-1]} record with ID: {model_id}")
         return {"ok": True}
--- rapid_db/crud/rapid_logging.py
+++ rapid_db/crud/rapid_logging.py
@@ -1,6 +1,10 @@
+import logging
 from sqlmodel import Session
 from ..models.rapid_logging import StageLog, StageLogMessage
 from datetime import datetime
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.WARNING)
 
 
 class StageLogCrud:
@@ -10,10 +14,12 @@ class StageLogCrud:
     def open_stage_log(
         self, session: Session, stage_log: StageLog.Open
     ) -> StageLog.Return:
+        logger.info("Opening a new stage_log")
         db_stage_log = StageLog.model_validate(stage_log)
         session.add(db_stage_log)
         session.commit()
         session.refresh(db_stage_log)
+        logger.info(f"Opened a new stage_log with ID: {db_stage_log.id}")
         return db_stage_log
 
     def get_stage_log_on_id(
@@ -21,6 +27,7 @@ class StageLogCrud:
     ) -> StageLog.Return:
         db_stage_log = session.get(StageLog, stage_log_id)
         if not db_stage_log:
+            logger.warning(f"No {self.name[:-1]} found with ID: {stage_log_id}")
             raise ValueError(
                 f"404: {self.name[:-1]} with ID: {stage_log_id} not found."
             )
@@ -29,6 +36,7 @@ class StageLogCrud:
     def close_stage_log(
         self, session: Session, stage_log: StageLog.Close
     ) -> StageLog.Return:
+        logger.info(f"Closing stage_log with ID: {stage_log.id}")
         db_stage_log = self.get_stage_log_on_id(session, stage_log.id)
         stage_log_data = stage_log.model_dump(exclude_unset=True)
 
@@ -39,15 +47,18 @@ class StageLogCrud:
         session.add(db_stage_log)
         session.commit()
         session.refresh(db_stage_log)
+        logger.info(f"Closed stage_log with ID: {stage_log.id}")
         return db_stage_log
 
     def delete_stage_log(self, session: Session, stage_log_id: int) -> dict:
         db_stage_log = self.get_stage_log_on_id(
             session=session, stage_log_id=stage_log_id
         )
+        logger.info(f"Deleting stage_log with ID: {stage_log_id}")
 
         session.delete(db_stage_log)
         session.commit()
+        logger.info(f"Deleted stage_log with ID: {stage_log_id}")
 
         return {"ok": True}
 
@@ -62,10 +73,16 @@ class StageLogMessageCrud:
     def add_stage_log_message(
         self, session: Session, stage_log_message: StageLogMessage.Add
     ) -> StageLogMessage.Return:
+        logger.info(
+            f"Adding a new stage_log_message for stage_log with ID: {stage_log_message.stage_log_id}"
+        )
         db_stage_log_message = StageLogMessage.model_validate(stage_log_message)
         session.add(db_stage_log_message)
         session.commit()
         session.refresh(db_stage_log_message)
+        logger.info(
+            f"Added a new stage_log_message for stage_log with ID: {stage_log_message.stage_log_id}"
+        )
         return db_stage_log_message
 
     def get_stage_log_message_on_id(
@@ -73,6 +90,7 @@ class StageLogMessageCrud:
     ) -> StageLogMessage.Return:
         db_stage_log_message = session.get(StageLogMessage, stage_log_message_id)
         if not db_stage_log_message:
+            logger.warning("No {self.name[:-1]} with ID: {stage_log_message_id} found.")
             raise ValueError(
                 f"404: {self.name[:-1]} with ID: {stage_log_message_id} not found."
             )
@@ -84,9 +102,11 @@ class StageLogMessageCrud:
         db_stage_log_message = self.get_stage_log_message_on_id(
             session=session, stage_log_message_id=stage_log_message_id
         )
+        logger.info(f"Deleting {self.name[:-1]} with ID: {stage_log_message_id}")
 
         session.delete(db_stage_log_message)
         session.commit()
+        logger.info(f"Deleted {self.name[:-1]} with ID: {stage_log_message_id}")
 
         return {"ok": True}
 
